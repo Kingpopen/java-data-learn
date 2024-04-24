7b0c147260c9e76c5feddf560cdfe2c8632e25ad
kingpopen <kingpopen@163.com>
2024-04-24 18:46:40 +0800

    feat: 添加jdbc的事务学习

---
+++ jdbc-learn/src/main/java/com/kingpopen/JdbcWithTransactionUtil.java
@@ -0,0 +1,76 @@
+package com.kingpopen;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.Objects;
+import java.util.Properties;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 彭锦波
+ * @project java-data-learn
+ * @description 含有事务操作的jdbc 工具类
+ * @date 2024/4/24 12:21:38
+ */
+@Slf4j
+public class JdbcWithTransactionUtil {
+
+  private static final DataSource dataSource;
+  private static final ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
+
+  // 使用static 块初始化
+  static {
+    try {
+      // 构造properties
+      Properties properties = new Properties();
+      InputStream is = JdbcWithTransactionUtil.class.getClassLoader()
+          .getResourceAsStream("db.properties");
+      properties.load(is);
+      dataSource = DruidDataSourceFactory.createDataSource(properties);
+    } catch (Exception e) {
+      log.error("构造连接池出现异常!");
+      throw new RuntimeException("构造连接池出现异常！", e);
+    }
+  }
+
+  // 获取连接
+  public static Connection getConnection() {
+    try {
+      // 1. 获取连接
+      Connection connection = threadLocal.get();
+      if (Objects.isNull(connection)) {
+        connection = dataSource.getConnection();
+        threadLocal.set(connection);
+      }
+      // 2. 设置为手动提交事务
+      connection.setAutoCommit(false);
+      return connection;
+    } catch (Exception e) {
+      // TODO: 2024/4/24
+      throw new RuntimeException("xxxx");
+    }
+  }
+
+  // 释放连接
+  public static void release() {
+    try {
+      Connection connection = threadLocal.get();
+      if (Objects.nonNull(connection)) {
+        threadLocal.remove();
+        /*
+         * 这个地方使用的是 DruidPooledConnection类，它一个代理类，这个地方代理的应该是JdbcConnection
+         * 它重写了close方法，这意味着这个地方的connection.close() 并不意味着连接真的被关闭了，
+         * 而是可能将它重新放到连接池中去了。
+         */
+        connection.setAutoCommit(true);
+        connection.close();
+      }
+    } catch (Exception e) {
+      // todo
+      throw new RuntimeException("");
+    }
+
+  }
+}
---
+++ jdbc-learn/src/test/java/com/kingpopen/JdbcWithTransactionTest.java
@@ -0,0 +1,81 @@
+package com.kingpopen;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 彭锦波
+ * @project java-data-learn
+ * @description 测试
+ * @date 2024/4/24 12:55:17
+ */
+@Slf4j
+public class JdbcWithTransactionTest {
+
+  // 测试一下jdbc的事务功能
+  @Test
+  public void testJdbcTransaction() {
+    Connection connection = JdbcWithTransactionUtil.getConnection();
+    try {
+      JdbcCrud api = new JdbcCrud();
+      // 1. 执行插入
+      api.insertPrepare(connection);
+
+      // 人为制造一个异常
+      int i = 1 / 0;
+
+      // 2. 执行删除
+      api.deletePrepare(connection);
+      // 3. 提交
+      connection.commit();
+
+      // 4. 进行查询
+      api.queryPrepare(connection);
+    } catch (Exception e) {
+      try {
+        // 回滚
+        connection.rollback();
+      } catch (SQLException ex) {
+        throw new RuntimeException(ex);
+      }
+      log.error("执行操作出现异常，进行回滚！", e);
+    } finally {
+      // 5. 释放连接
+      JdbcWithTransactionUtil.release();
+    }
+  }
+
+  // 多个次请求是否会出现异常
+  @Test
+  public void testJdbcTransactionHighConcurrency(){
+    JdbcCrud api = new JdbcCrud();
+    Connection connection = JdbcWithTransactionUtil.getConnection();
+    log.info("======> connection is:{}", connection);
+    // 正常插入数据
+    try{
+      api.insertPrepare(connection);
+      connection.commit();
+    }catch (Exception e){
+      throw new RuntimeException("出现异常！", e);
+    }finally {
+      JdbcWithTransactionUtil.release();
+    }
+
+
+    // 发生回滚的情况
+    connection = JdbcWithTransactionUtil.getConnection();
+    log.info("======> connection is:{}", connection);
+    try{
+      api.insertPrepare(connection);
+      connection.rollback();
+    }catch (Exception e){
+      throw new RuntimeException("出现异常！", e);
+    }
+
+    // 进行查询
+    api.queryPrepare(connection);
+  }
+}
