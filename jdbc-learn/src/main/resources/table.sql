c314037be9c11d01c6f435ac08dd38954b4eb4d6
kingpopen <kingpopen@163.com>
2024-04-29 23:28:26 +0800

    refactor(jdbc): 修改sql脚本

--- jdbc-learn/src/main/resources/table.sql
+++ jdbc-learn/src/main/resources/table.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `identity_id` varchar(100) DEFAULT NULL COMMENT '身份证号',
   `address` varchar(100) DEFAULT NULL COMMENT '地址',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- mydatabase.user_tmp definition
 CREATE TABLE IF NOT EXISTS `user_tmp` (
@@ -15,4 +15,4 @@ CREATE TABLE IF NOT EXISTS `user_tmp` (
   `address` varchar(100) DEFAULT NULL COMMENT '地址',
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_key` (`username`,`address`,`identity_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
